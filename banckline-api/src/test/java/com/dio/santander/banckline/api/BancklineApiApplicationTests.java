e709b1cb4d5b678296b8fdaba11229548ca0ff68
fsclaudio <fsclaudio@gmail.com>
2022-05-03 11:00:25 -0300

    init

---
+++ banckline-api/src/test/java/com/dio/santander/banckline/api/BancklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander.banckline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
